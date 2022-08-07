be8cedbe74194c8f390e8ba1bf45eed6c829d249
BormsKato <kato.borms@student.howest.be>
2022-08-07 14:34:49 +0200

    test

---
+++ backend/test1.py
@@ -0,0 +1,15 @@
+import serial
+import time
+
+try:
+  while True:
+    ser = serial.Serial('/dev/ttyS0',baudrate=9600,timeout=3.0)
+    time.sleep(2)
+    ser.write(b'Hello')
+    a=ser.readline()
+    print(a)
+    ser.close()
+except KeyboardInterrupt as e:
+  print(e)
+finally:
+  print("Script stopped")
