76ec9376d805c92cb912230df7cfd776774d5d9c
BormsKato <kato.borms@student.howest.be>
2022-08-18 15:23:04 +0200

    dms gps omgezet naar juiste waarde

--- backend/test1.py
+++ backend/test1.py
@@ -13,4 +13,18 @@ while True:
 
 	if properdata[0:5] == "GNGGA":
 		print(properdata)
+		latitude= properdata[16:26]
+		Dlat= latitude[0:2]
+		Mlat = latitude[2:4]
+		Slat =latitude[5:7]
+		latitudeWaarde = int(Dlat) + (int(Mlat)/60) + (int(Slat)/3600)
+		print(latitudeWaarde)
+		longitude= properdata[31:40]
+		Dlong= longitude[0:1]
+		Mlong = longitude[1:3]
+		Slong =longitude[4:6]
+		longitudeWaarde = int(Dlong) + (int(Mlong)/60) + (int(Slong)/3600)
+		print(longitudeWaarde)
+
+
 
