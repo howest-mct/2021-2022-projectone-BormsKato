d3c2b1414df657ee218187130e056ebe865b63e8
BormsKato <kato.borms@student.howest.be>
2022-08-24 10:17:12 +0200

    delete svg ok

--- front-end/script/app.js
+++ front-end/script/app.js
@@ -35,8 +35,54 @@ const fill_table_horses = function (jsonObject) {
         <td class="c-cell_second">${data.HorseId}</td>
         <td class="c-cell_second">${data.naam}</td>
         <td class="c-cell_second">${data.leeftijd}</td>
-        <td><input type="button" value="Delete" onclick="deleteRow(${data.HorseId})"></td>
-      </tr>`
+        <td><button  onclick="deleteRow(${data.HorseId}, '${data.naam}')"> <svg version="1.1" width="18.75"
+        height="26.25" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        viewBox="0 0 330 330" style="enable-background:new 0 0 330 330;" xml:space="preserve">
+     <g>
+       <path d="M240,121.076V90h15c8.284,0,15-6.716,15-15s-6.716-15-15-15h-30h-15V15c0-8.284-6.716-15-15-15H75c-8.284,0-15,6.716-15,15
+         v45H45H15C6.716,60,0,66.716,0,75s6.716,15,15,15h15v185c0,8.284,6.716,15,15,15h60h37.596c19.246,24.347,49.031,40,82.404,40
+         c57.897,0,105-47.103,105-105C330,172.195,290.817,128.377,240,121.076z M90,30h90v30h-15h-60H90V30z M105,260H60V90h15h30h60h30
+         h15v31.076c-50.817,7.301-90,51.119-90,103.924c0,12.268,2.122,24.047,6.006,35H105z M225,300c-41.355,0-75-33.645-75-75
+         s33.645-75,75-75s75,33.645,75,75S266.355,300,225,300z"/>
+       <path d="M256.819,193.181c-5.857-5.857-15.355-5.857-21.213,0L225,203.787l-10.606-10.606c-5.857-5.857-15.355-5.857-21.213,0
+         c-5.858,5.857-5.858,15.355,0,21.213L203.787,225l-10.606,10.606c-5.858,5.857-5.858,15.355,0,21.213
+         c2.929,2.929,6.768,4.394,10.606,4.394s7.678-1.465,10.606-4.394L225,246.213l10.606,10.606c2.929,2.929,6.768,4.394,10.606,4.394
+         s7.678-1.465,10.606-4.394c5.858-5.857,5.858-15.355,0-21.213L246.213,225l10.606-10.606
+         C262.678,208.536,262.678,199.038,256.819,193.181z"/>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     <g>
+     </g>
+     </svg> </button>
+</td>
+     </tr>`
   }
   document.querySelector('.js-table-horses').innerHTML = htmlString;
 
@@ -67,6 +113,7 @@ const error_get = function () {
   let htmlString = `  <td class="c-cell_second">Error</td>
                     <td class="c-cell_second">Error</td>
                     <td class="c-cell_second">Error</td>`;
+                    
   document.querySelector('.js-table-horses').innerHTML = htmlString
 }
 
@@ -78,10 +125,23 @@ const error_get_history = function () {
 
 }
 
-const deleteRow = function(paard){
-  console.log("deleteknop")
+const deleteRow = function(paard, naam){
+  // console.log("deleteknop")
+  // console.log(paard)
+  // socket.emit('F2B_deletehorse', paard)
   console.log(paard)
-  socket.emit('F2B_deletehorse', paard)
+  console.log(naam)
+  let text = "Are you sure you want to delete " + naam +"?";
+  if (confirm(text) == true) {
+    console.log('delete' + {paard})
+    socket.emit('F2B_deletehorse', paard);
+    const url = backend + `/horses/`
+    handleData(url, fill_table_horses, error_get)
+  } else {
+    console.log("cancel" )
+    const url = backend + `/horses/`
+    handleData(url, fill_table_horses, error_get)
+  }
 }
 
 
